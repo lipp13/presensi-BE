1743bb3c3b170312df702ef3fa5611f97575b6b1
Alif Alfathar <lipsft53@gmail.com>
2026-09-04 14:33:50 +0700

    fix: import errorResponse in app.js for telegram health check

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { successResponse } = require("./utils/response");
+const { successResponse, errorResponse } = require("./utils/response");
 const { notFoundHandler, errorHandler } = require("./middlewares/error.middleware");
 const { testConnection, isConfigured } = require("./config/supabase");
 
